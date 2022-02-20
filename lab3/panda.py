f56ad7d6761297f066e0815d711351dcf87455b8
Arina Efimova <wildfire.aei@gmail.com>
2022-02-20 18:30:32 +0300

    drew panda

--- lab3/panda.py
+++ lab3/panda.py
@@ -8,9 +8,10 @@ screen = pygame.display.set_mode((600, 600))
 
 CORAL = (245, 155, 141)
 BAMBOO = (54, 117, 50)
-WHITE = (255, 248, 250)
+WHITE = (255, 238, 250)
 REALWHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREY = (42, 31, 47)
 BEIGE = (250, 235, 215)
 
 dr.rect(screen, CORAL, (0, 0, 600, 600)) #wallpaper
@@ -46,14 +47,24 @@ dr.polygon(screen, BLACK, [[290,440], [340,500], [310,530], [250,520]])
 dr.ellipse(screen, BLACK, [250, 510, 65,30])
 dr.ellipse(screen, BLACK, [245, 480, 70, 60])
 
-dr.ellipse(screen, REALWHITE, (200,230, 150, 100)) #head
-dr.polygon(screen, REALWHITE, [[200,270], [240, 365], [280, 365], [340,270]])
+#rightear
+dr.rect(screen, BLACK, (210, 215, 23 , 45), border_top_right_radius = 25, border_bottom_left_radius = 40, border_bottom_right_radius = 40, border_top_left_radius = 80)
+
+dr.ellipse(screen, REALWHITE, (205, 245, 120, 100)) #head
+#dr.polygon(screen, REALWHITE, [[205,270], [240, 365], [280, 365], [300,270]])
 dr.ellipse(screen, REALWHITE, [225,275, 99,90])
 dr.ellipse(screen, REALWHITE, [195,240, 100,125])
 
 dr.ellipse(screen, BEIGE, [190, 310, 70, 50]) #face
 dr.polygon(screen, BEIGE, [[210, 300],[240,310],[240, 340],[190,335]])
-dr.rect(screen, BLACK, (240, 300, 30, 30), border_top_right_radius = 15, border_bottom_left_radius = 20, border_bottom_right_radius = 5, border_top_left_radius = 10)
+dr.rect(screen, BLACK, (230, 295, 30, 30), border_top_right_radius = 20, border_bottom_left_radius = 20, border_bottom_right_radius = 7, border_top_left_radius = 10)
+dr.rect(screen, BLACK, (200,295, 10,20), border_top_right_radius = 5, border_bottom_left_radius = 5, border_bottom_right_radius = 5, border_top_left_radius = 5)
+dr.rect(screen, BLACK, (190, 325, 20 , 15), border_top_right_radius = 5, border_bottom_left_radius = 10, border_bottom_right_radius = 10, border_top_left_radius = 5)
+dr.rect(screen, BLACK, (200, 345, 25 , 15), border_top_right_radius = 5, border_bottom_left_radius = 20, border_bottom_right_radius = 25, border_top_left_radius = 7)
+
+#leftear
+dr.rect(screen, GREY, (270, 230, 25 , 35), border_top_right_radius = 20, border_bottom_left_radius = 25, border_bottom_right_radius = 25, border_top_left_radius = 30)
+dr.rect(screen, BLACK, (260, 225, 25 , 45), border_top_right_radius = 5, border_bottom_left_radius = 10, border_bottom_right_radius = 20, border_top_left_radius = 20)
 
 
 pygame.display.update()
