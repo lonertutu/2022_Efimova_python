187fc97d73b9285737eb18ae5642c0b39d3ee451
Arina Efimova <wildfire.aei@gmail.com>
2022-03-15 17:11:18 +0300

    Well done! I add score on the screen, two types of symbols

--- lab6 (catch ball)/main.py
+++ lab6 (catch ball)/main.py
@@ -150,11 +150,13 @@ while not finished:
     speed_effect_planets()
     collision_detection_planets()
 
-    f1 = pygame.font.Font(None, 72)
-    text1 = f1.render('Hello Привет', 1, (180, 0, 0))
 
-    screen.blit(text1, (10, 50))
 
     screen.fill(BLACK)
+
+    f1 = pygame.font.Font(None, 36)
+    text1 = f1.render(str(score) , 1, (180, 0, 0))
+
+    screen.blit(text1, (10, 50))
 
 pygame.quit()
